e850a5e7d98565ac312981e5861a8e5742b974c1
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-25 15:15:56 +0530

    did the email map task and added the comments.

--- Week-2/src/Main.java
+++ Week-2/src/Main.java
@@ -9,19 +9,26 @@ public class Main {
         abc@gmail.com, harsh@yahoo.com, javed@hotmail.com, johndoe@yahoo.com, johnyB@gmail.com,  johndoe@hotmail.com
         , hello@gmail.com, mike@yahoo.com
         """;
-
+        //splitting the emails
         String[] emails = str.split(",");
+
+        // creating the map of emails to list of emails.
         Map<String, List<String>> map = new HashMap<>();
         for (String x : emails) {
+            // string builder to remove spaces and effectively getting substrings.
             StringBuilder sb = new StringBuilder(x.strip());
+            // checking the index if @ for cutting the email provider from the string
             int separatorStart = sb.indexOf("@");
             if (separatorStart != -1) {
                 String provider = sb.substring(separatorStart + 1);
+                //cutting the substring from @ to .
                 int separatorEnd = provider.indexOf(".");
                 if (separatorEnd != -1) {
                     provider = provider.substring(0, separatorEnd);
+                    // check if the provider exists if yes then add if no add to a new list.
                     List<String> emailList = map.getOrDefault(provider.toLowerCase(), new ArrayList<>());
                     emailList.add(sb.toString());
+                    // adding to map after converting to lowercase.
                     map.put(provider.toLowerCase(), emailList);
                 }
             }
