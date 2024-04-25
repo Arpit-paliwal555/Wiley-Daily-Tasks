84b092469fd9ea698da04a2ae160887e90ca4e84
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-25 15:07:22 +0530

    did the email map task.

--- Week-2/src/Main.java
+++ Week-2/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = """
+        abc@gmail.com, harsh@yahoo.com, javed@hotmail.com, johndoe@yahoo.com, johnyB@gmail.com,  johndoe@hotmail.com
+        , hello@gmail.com, mike@yahoo.com
+        """;
+
+        String[] emails = str.split(",");
+        Map<String, List<String>> map = new HashMap<>();
+        for (String x : emails) {
+            StringBuilder sb = new StringBuilder(x.strip());
+            int separatorStart = sb.indexOf("@");
+            if (separatorStart != -1) {
+                String provider = sb.substring(separatorStart + 1);
+                int separatorEnd = provider.indexOf(".");
+                if (separatorEnd != -1) {
+                    provider = provider.substring(0, separatorEnd);
+                    List<String> emailList = map.getOrDefault(provider.toLowerCase(), new ArrayList<>());
+                    emailList.add(sb.toString());
+                    map.put(provider.toLowerCase(), emailList);
+                }
+            }
+        }
+        System.out.println("Gmail: "+map.get("gmail"));
+        System.out.println("Yahoo: "+map.get("gmail"));
+        System.out.println("Hotmail: "+map.get("gmail"));
+
     }
 }
