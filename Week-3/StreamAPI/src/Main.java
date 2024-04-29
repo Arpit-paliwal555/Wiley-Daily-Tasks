20343f437985a5877a679f6bbe5f90debe43d208
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-29 15:26:53 +0530

    correct one.

--- Week-3/StreamAPI/src/Main.java
+++ Week-3/StreamAPI/src/Main.java
@@ -16,15 +16,6 @@ public class Main {
         List<String> ans = new ArrayList<>();
         names.forEach((a)->{
             String[] check = a.split(" ");
-            ans.add(check[0]);
-
-        });
-        return ans;
-    }
-    static List<String> getAllNames(List<String> names){
-        List<String> ans = new ArrayList<>();
-        names.forEach((a)->{
-            String[] check = a.split(" ");
             String temp = "";
             for (String s:check){
                 temp += Character.toUpperCase(s.charAt(0));
@@ -35,15 +26,19 @@ public class Main {
         });
         return ans;
     }
+    static List<String> getAllNames(List<String> names){
+        List<String> ans1 = getValidNames(names);
+        List<String> ans2 = getShortNames(ans1);
+        return ans2;
+    }
 
     public static void main(String[] args) {
         List<String> names = List.of("Harsh Kumar","Virat Kohli", "Mahendra Singh Dhoni","Akash","Karan Chopra");
-        List<String> validNames = getValidNames(names);
-        List<String> shortNames = getShortNames(validNames);
+       // List<String> validNames = getValidNames(names);
+       // List<String> shortNames = getShortNames(validNames);
         List<String> allName = getAllNames(names);
 
-        System.out.println("valid Names:"+validNames);
-        System.out.println("Short Names:"+shortNames);
+
         System.out.println("All short names (initials):"+allName);
     }
 }
