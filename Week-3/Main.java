54c73cb4d9165d625472a184a90ee9b9cd4bffe5
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-29 14:31:04 +0530

    For each list task (with replace all).

--- Week-3/Main.java
+++ Week-3/Main.java
@@ -4,9 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>(List.of("javed","jimmy","johny"));
-        for(int i=0;i<names.size();i++){
-            names.set(i,names.get(i).toUpperCase());
-        }
+        names.replaceAll(String::toUpperCase);
         System.out.println(names);
     }
 
