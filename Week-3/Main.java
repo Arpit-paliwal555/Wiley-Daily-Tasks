a3f8ac4a3872278a079dbcf3abfee6a2346640d6
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-29 15:07:34 +0530

    Names task done

--- Week-3/Main.java
+++ Week-3/Main.java
@@ -4,7 +4,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>(List.of("javed","jimmy","johny"));
-        names.replaceAll(String::toUpperCase);
+        for(int i=0;i<names.size();i++){
+            names.set(i,names.get(i).toUpperCase());
+        }
         System.out.println(names);
     }
 
---
+++ Week-3/StreamAPI/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static List<String> getValidNames(List<String> names){
+        List<String> ans = new ArrayList<>();
+        names.forEach((a)->{
+            String[] check = a.split(" ");
+            if (check.length==2){
+                ans.add(a);
+            }
+        });
+        return ans;
+    }
+    static List<String> getShortNames(List<String> names){
+        List<String> ans = new ArrayList<>();
+        names.forEach((a)->{
+            String[] check = a.split(" ");
+            ans.add(check[0]);
+
+        });
+        return ans;
+    }
+    static List<String> getAllNames(List<String> names){
+        List<String> ans = new ArrayList<>();
+        names.forEach((a)->{
+            String[] check = a.split(" ");
+            String temp = "";
+            for (String s:check){
+                temp += Character.toUpperCase(s.charAt(0));
+                temp += ".";
+            }
+            ans.add(temp);
+
+        });
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        List<String> names = List.of("Harsh Kumar","Virat Kohli", "Mahendta Singh Dhoni","Akash","Karan Chopra");
+        List<String> validNames = getValidNames(names);
+        List<String> shortNames = getShortNames(validNames);
+        List<String> allName = getAllNames(names);
+
+        System.out.println("valid Names:"+validNames);
+        System.out.println("Short Names:"+shortNames);
+        System.out.println("All short names (initials):"+allName);
+    }
+}
