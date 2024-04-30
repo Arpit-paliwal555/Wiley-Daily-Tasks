b912388f6b58b04bf322e5d5c1fa2685e6428c47
Arpit-paliwal555 <paliwalarpit93@gmail.com>
2024-04-30 10:49:03 +0530

    Employee task with stream done with map.

--- Week-3/StreamAPI/src/EmployeeMain.java
+++ Week-3/StreamAPI/src/EmployeeMain.java
@@ -1,6 +1,5 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class EmployeeMain {
     public static void main(String[] args) {
@@ -10,15 +9,24 @@ public class EmployeeMain {
         Employee emp4 = new Employee(104,"Rituraj",55000, LocalDate.parse("2022-06-20"));
         Employee emp5 = new Employee(107,"John",55000, LocalDate.parse("2021-08-12"));
 
-        List<Employee> list = new ArrayList<>(List.of(emp1,emp2,emp3,emp4,emp5));
+        //List<Employee> list = new ArrayList<>(List.of(emp1,emp2,emp3,emp4,emp5));
+        Map<Integer,Employee> map = new HashMap<>();
         int giveniId = 101;
+        String name = "Karan";
+        map.put(101,emp1);
+        map.put(103,emp2);
+        map.put(102,emp3);
+        map.put(104,emp4);
+        map.put(107,emp5);
 
-        Employee empWithGivenID = list.stream().filter(e->e.id==giveniId).findFirst().orElse(null);
-        list.stream().forEach(employee -> {
-            employee.salary += (employee.salary/100)*20;
-        });
-        System.out.println("Employee with id 101: "+empWithGivenID);
-        System.out.println(list);
+        Employee empWithGivenName = map.values().stream().filter(e-> e.name.equalsIgnoreCase(name)).findFirst().orElse(null);
+//        list.forEach(employee -> {
+//            if(employee.equals(empWithGivenName))
+//                employee.salary += (employee.salary/100)*20;
+//        });
+        //empWithGivenID.salary += (empWithGivenID.salary/100)*20;
+        System.out.println("Employee with id 101: "+empWithGivenName);
+        //System.out.println(list);
 
     }
 }
